43dbf1c5a245b60bfd91f2f0a3d569f93bb51acf
YamilaAbad <yamiabad@hotmail.com>
2023-10-19 15:44:35 -0300

    servicio monopatines - estructura

---
+++ monopatin-service/src/main/java/com/monopatin/monopatinservice/MonopatinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.monopatin.monopatinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopatinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopatinServiceApplication.class, args);
+	}
+
+}
---
+++ monopatin-service/src/test/java/com/monopatin/monopatinservice/MonopatinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monopatin.monopatinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopatinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
